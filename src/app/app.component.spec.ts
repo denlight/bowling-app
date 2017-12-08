c13f8ed7504c8d2868317cec2d6407fbbffe9208
Denys <dfedotov@solutionstream.com>
2017-12-07 23:43:49 -0700

    added initial set of tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -10,4 +10,29 @@ describe('App', () => {
     let fixture = TestBed.createComponent(AppComponent);
     expect(fixture.componentInstance instanceof AppComponent).toBe(true, 'should create AppComponent');
   });
+
+  describe('Playing a frame', () => {
+    it('should return a sum of pins after the frame is played(2 throws)', () => {
+    
+    });
+  });
+  describe('Handling a SPARE', () => {
+    it('should get awarded ten pins, plus a bonus of whatever is scored with the next ball', () => {
+
+    });
+  });
+  describe('Handling a STRIKE', () => {
+    it('should get awarded ten pins, plus a bonus of whatever is scored with the next 2 balls', () => {
+      
+    });
+  });
+  describe('Tenth frame', () => {
+    it('should allow to throw two extra balls for achieving a STRIKE', () => {
+
+    });
+
+    it('should allow to throw one extra ball for achieving a SPARE', () => {
+      
+    });
+  });
 });
