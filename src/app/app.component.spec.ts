33d9b7fabc00de0d4c610642e71b6fc6fba31dfc
Denys <dfedotov@solutionstream.com>
2017-12-09 21:09:15 -0700

    covered tests with code but it looks like I missunderstood the whole 10th frame thing.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
-describe('App', () => {
+describe('BOWLING APP: ', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({ declarations: [AppComponent]});
   });
@@ -22,68 +22,111 @@ describe('App', () => {
     constructor(throw1?: number, throw2?: number) {
       this.throw1 = throw1 || 0;
       this.throw2 = throw2 || 0;
-      this.total = throw1 + throw2;
-      this.isSpare = throw1 < 10 && throw1 + throw2 === 10;
+      this.total = this.throw1 + this.throw2;
+      this.isSpare = this.throw1 < 10 && this.throw1 + this.throw2 === 10;
       this.isStrike = throw1 === 10;
       this.totalIsFinal = !this.isSpare && !this.isStrike;
     }
   }
 
-  let score: Frame[] = [];
+  let frames: Frame[] = [];
 
   let total: number;
 
-  let playFrame = (throw1?: number, throw2?: number) => {
-    score.push(new Frame(throw1, throw2));
+  let recalculateFramesTotals = () => {
+    total = 0;
+    frames.forEach((frame,i) => {
+      if(!frame.totalIsFinal) {
+        // handle spares
+        if(frame.isSpare && frames[i+1]) {
+          frame.total += frames[i+1].throw1;
+          frame.totalIsFinal = true;
+        }
+        // handle strikes
+        if(frame.isStrike && frames[i+1] && !frames[i+1].isStrike) {
+          frame.total += frames[i+1].total;
+          frame.totalIsFinal = true;
+        }
+        if(frame.isStrike && frames[i+1] && frames[i+1].isStrike && frames[i+2]) {
+          frame.total += frames[i+1].total + frames[i+2].throw1;
+          frame.totalIsFinal = true;
+        }
+      }
+      total += frame.total;
+    });
   };
 
-  describe('Playing a frame', () => {
+  let playFrame = (throw1?: number, throw2?: number) => {
+    let gameOver: boolean;
+    frames.push(new Frame(throw1, throw2));
+    recalculateFramesTotals();
+    // determine wheather the game is over
+    if(frames[9] && (frames[9].isSpare && frames.length === 11
+      || frames[9].isStrike && frames.length === 12)) {
+      gameOver = true;
+    }
+    return gameOver;
+  };
+
+  describe('Playing a frame: ', () => {
     beforeEach(() => {
-      score = [];
+      frames = [];
       total = 0;
     });
-
     it('should return a sum of pins after the frame is played(2 throws) and identify STRIKE or SPARE', () => {
       // gutter
       playFrame();
-      expect(score[0].total).toBe(0);
-      expect(score[0].isSpare).toBe(false);
-      expect(score[0].isStrike).toBe(false);
+      expect(frames[0].total).toBe(0);
+      expect(frames[0].isSpare).toBe(false);
+      expect(frames[0].isStrike).toBe(false);
       // meh
       playFrame(2,4);
-      expect(score[1].total).toBe(6);
-      expect(score[1].isSpare).toBe(false);
-      expect(score[1].isStrike).toBe(false);
+      expect(frames[1].total).toBe(6);
+      expect(frames[1].isSpare).toBe(false);
+      expect(frames[1].isStrike).toBe(false);
       // SPARE
       playFrame(4,6);
-      expect(score[2].total).toBe(10);
-      expect(score[2].isSpare).toBe(true);
-      expect(score[2].isStrike).toBe(false);
+      expect(frames[2].total).toBe(10);
+      expect(frames[2].isSpare).toBe(true);
+      expect(frames[2].isStrike).toBe(false);
       // STRIKE
       playFrame(10,0);
-      expect(score[3].total).toBe(10);
-      expect(score[3].isSpare).toBe(false);
-      expect(score[3].isStrike).toBe(true);
+      expect(frames[3].total).toBe(10);
+      expect(frames[3].isSpare).toBe(false);
+      expect(frames[3].isStrike).toBe(true);
     });
   });
 
-  describe('Handling a SPARE', () => {
-    it('should get awarded ten pins, plus a bonus of whatever is scored with the next ball', () => {
+  describe('Handling a SPARE: ', () => {
+    beforeEach(() => {
+      frames = [];
+      total = 0;
+    });
+    it('should get awarded ten pins, plus a bonus of whatever is framesd with the next ball', () => {
       playFrame(4,6);
       playFrame(2,3);
-      expect(score[0].total).toBe(12);
+      expect(frames[0].total).toBe(12);
     });
   });
 
-  describe('Handling a STRIKE', () => {
-    it('should get awarded ten pins, plus a bonus of whatever is scored with the next 2 balls', () => {
+  describe('Handling a STRIKE: ', () => {
+    beforeEach(() => {
+      frames = [];
+      total = 0;
+    });
+    it('should get awarded ten pins, plus a bonus of whatever is framesd with the next 2 balls', () => {
       playFrame(10);
       playFrame(3,6);
-      expect(score[0].total).toBe(19);
+      expect(frames[0].total).toBe(19);
     });
   });
 
-  describe('Tenth frame', () => {
+  describe('Tenth frame: ', () => {
+    beforeEach(() => {
+      frames = [];
+      total = 0;
+    });
+
     it('should allow to throw two extra balls for achieving a STRIKE', () => {
       playFrame(1);
       playFrame(2);
@@ -110,12 +153,12 @@ describe('App', () => {
       playFrame(8);
       playFrame(9);
       playFrame(9,1); // tenth frame is a spare
-      expect(playFrame(10)).toBe(true); // playFrame will return FALSE by default until the game is over.
+      expect(playFrame(10)).toBe(true); // playFrame() will return FALSE by default until the game is over.
     });
   });
 
   describe('PERFECT GAME!', () => {
-    it('should return a total score of 300', () => {
+    it('should return a total frames of 300', () => {
       playFrame(10);
       playFrame(10);
       playFrame(10);
